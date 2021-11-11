d1a1dd3bba46748016165916722961f23d6eed2a
MINJUNG YUN <eminjae8659@gmail.com>
2021-11-11 22:12:41 +0900

    Time: 0 ms (100.00%), Space: 36 MB (47.57%) - LeetHub

---
+++ xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            nums[i] = start + 2*i;
+        }
+        
+        int result = 0;
+        for(int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+}
